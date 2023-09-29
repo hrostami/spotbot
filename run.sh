e4757990567d4612c41d426a77ff2ed8541b9468
hrostami <rostamimofrad@gmail.com>
2023-09-29 23:58:13 +0330

    added sh helper

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if ! command -v tmux &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y tmux
+fi
+
+if [ ! -d "spotbot" ]; then
+    git clone https://github.com/hrostami/spotbot.git
+else
+    cd spotbot
+    git pull origin master
+    cd ..
+fi
+
+if [ ! -d "spotbot" ]; then
+    python3 -m venv spotbot
+fi
+
+source spotbot/bin/activate
+pip install --upgrade python-telegram-bot==13.5 spotdl
+
+if ! command -v ffmpeg &> /dev/null; then
+    sudo apt-get install -y ffmpeg
+fi
+
+tmux has-session -t spotbot 2>/dev/null
+if [ $? != 0 ]; then
+    tmux new-session -d -s spotbot "python3 spotbot.py"
+else
+    tmux kill-session -t spotbot
+    tmux new-session -d -s spotbot "python3 spotbot.py"
+fi
